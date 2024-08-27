28ca9311ab0831c7cc36d5ad285fffbb2a649b4d
Mohamed Tobal <mohamedtobal369@gmail.com>
2024-08-27 11:05:54 +0300

    Tailwind And Test Get Element By ID

--- src/App.js
+++ src/App.js
@@ -2,7 +2,33 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="flex flex-col gap-5 p-5">
+      <span className="inline-flex -space-x-px overflow-hidden rounded-md border bg-white shadow-sm justify-center">
+        <button
+          data-testid='btn-1'
+          onClick={() => console.log("Fucken Hello1")}
+          className="inline-block px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50 focus:relative"
+        >
+          Edit
+        </button>
+
+        <button
+          data-testid='btn-2'
+          onClick={() => console.log("Fucken Hello2")}
+          className="inline-block px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50 focus:relative"
+        >
+          View
+        </button>
+
+        <button
+          data-testid='btn-3'
+          onClick={() => console.log("Fucken Hello3")}
+          className="inline-block px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50 focus:relative"
+        >
+          Delete
+        </button>
+      </span>
+
       <a
         className="App-link"
         href="https://reactjs.org"
--- src/App.test.js
+++ src/App.test.js
@@ -2,13 +2,15 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  // 1 - Render The Component
+  // 1 - Render The Component, Virtual Render
   render(<App />);
 
   // 2 - Find The Element For Test, every thing will put on the screen, and i tell it what i want to test
-  const myLink = screen.getByText(/learn react/i);
+  // const myLink = screen.getByText(/learn react/i);
+
+  const btnById = screen.getByTestId("btn-1")
 
   // 3 - expect and text methods are from json file, i name the file App.test.js
   // here i tell it to check if it exist toBeInTheDocument
-  expect(myLink).toBeInTheDocument();
+  expect(btnById).toBeInTheDocument();
 });
