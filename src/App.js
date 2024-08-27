7bbbd6a870344b8473784b70b2c233551e50ddfb
Mohamed Tobal <mohamedtobal369@gmail.com>
2024-08-27 10:13:30 +0300

    FIrst App Test

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      App
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,13 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
+  // 1 - Render The Component
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  // 2 - Find The Element For Test, every thing will put on the screen, and i tell it what i want to test
+  const myLink = screen.getByText(/learn react/i);
+
+  // 3 - expect and text methods are from json file, i name the file App.test.js
+  // here i tell it to check if it exist toBeInTheDocument
+  expect(myLink).toBeInTheDocument();
 });
